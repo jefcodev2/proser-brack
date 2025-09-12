2b54751acec61ee77ec748a594e8ae50a7c6da2f
Jefferson Pinchao <japinchaoc@gmail.com>
2025-09-12 00:29:35 -0500

    config deploy production

--- database/config.js
+++ database/config.js
@@ -1,56 +1,19 @@
 const pgPromise = require("pg-promise");
 require('dotenv').config();
 
-// Función para obtener la configuración según el entorno
-const getConfig = () => {
-    const env = process.env.NODE_ENV || 'development';
-    
-    switch (env) {
-        case 'development':
-            return {
-                host: process.env.DB_HOST_DEV ,
-                port: process.env.DB_PORT_DEV ,
-                database: process.env.DB_NAME_DEV ,
-                user: process.env.DB_USER_DEV ,
-                password: process.env.DB_PASSWORD_DEV 
-            };
-        
-        case 'test':
-            return {
-                host: process.env.DB_HOST_TEST ,
-                port: process.env.DB_PORT_TEST ,
-                database: process.env.DB_NAME_TEST ,
-                user: process.env.DB_USER_TEST ,
-                password: process.env.DB_PASSWORD_TEST 
-            };
-        
-        case 'production':
-            return {
-                host: process.env.DB_HOST_PROD ,
-                port: process.env.DB_PORT_PROD ,
-                database: process.env.DB_NAME_PROD ,
-                user: process.env.DB_USER_PROD ,
-                password: process.env.DB_PASSWORD_PROD 
-            };
-        
-        default:
-            console.warn(`Entorno no reconocido: ${env}. Usando configuración de desarrollo.`);
-            return {
-                host: process.env.DB_HOST_DEV ,
-                port: process.env.DB_PORT_DEV ,
-                database: process.env.DB_NAME_DEV ,
-                user: process.env.DB_USER_DEV ,
-                password: process.env.DB_PASSWORD_DEV 
-            };
-    }
+// Configuración usando DATABASE_URL
+const config = {
+   /*  host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD */
+    connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_rOKGT8cS1WIU@ep-late-boat-accan2fo-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require'
 };
 
-const config = getConfig();
-
-if (process.env.NODE_ENV === 'development') {
-    console.log(`Conectando a la base de datos en entorno: ${process.env.NODE_ENV}`);
-    console.log(`Host: ${config.host}, Puerto: ${config.port}, Base de datos: ${config.database}`);
-}
+console.log(`Conectando a la base de datos:`);
+//console.log(`Host: ${config.host}, Puerto: ${config.port}, Base de datos: ${config.database}`);
+console.log(`DATABASE_URL: ${config.connectionString}`);
 
 const pgp = pgPromise({});
 const db_postgres = pgp(config);
